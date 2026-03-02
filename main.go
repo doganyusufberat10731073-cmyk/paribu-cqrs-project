645e92b4396bba1956ef19c48991a7f86a7e5b7c
yusufberatdogan <doganyusufberat10731073@gmail.com>
2026-03-02 16:28:41 +0300

    Remove analysis paralysis message from output

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Merhaba Anıl Bey, Paribu CQRS/Event Sourcing projesi başlıyor. Analysis Paralysis yenildi!")
+    fmt.Println("Merhaba Anıl Bey, Paribu CQRS/Event Sourcing projesi başlıyor.")
 }
